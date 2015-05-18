a204cba9d7ae6cb70b52d53d6cb4fe6caa310569
Benjamin Toll <benjam72@yahoo.com>
2015-05-18 15:09:20 -0400

    Converted tabs to spaces in python scripts in build/
    http://stackoverflow.com/a/11094620
    find . -name '*.java' ! -type d -exec bash -c 'expand -t 4 "$0" > /tmp/e && mv /tmp/e "$0"' {} \;

--- build/build.py
+++ build/build.py
@@ -24,30 +24,30 @@ def usage(level):
     print(textwrap.dedent(str))
 
 def main(argv):
-	JAR_FILE = "/path/to/yuicompressor-2.4.8.jar"
-	SRC_DIR = "/path/to/src/js/"
-	DEST_DIR = "."
+    JAR_FILE = "/path/to/yuicompressor-2.4.8.jar"
+    SRC_DIR = "/path/to/src/js/"
+    DEST_DIR = "."
 
-	try:
-		opts, args = getopt.getopt(argv, "hv:", ["help", "version=", "jar=", "src_dir=", "dest_dir="])
-	except getopt.GetoptError:
-		usage(0)
-		sys.exit(2)
-	for opt, arg in opts:
-		if opt in ("-h", "--help"):
-			usage(1)
-			sys.exit(0)
-		elif opt in ("-v", "--version"):
-			VERSION = arg
-		elif opt == "--jar":
-			JAR_FILE = arg
-		elif opt == "--src_dir":
-			SRC_DIR = arg
-		elif opt == "--dest_dir":
-			DEST_DIR = arg
+    try:
+        opts, args = getopt.getopt(argv, "hv:", ["help", "version=", "jar=", "src_dir=", "dest_dir="])
+    except getopt.GetoptError:
+        usage(0)
+        sys.exit(2)
+    for opt, arg in opts:
+        if opt in ("-h", "--help"):
+            usage(1)
+            sys.exit(0)
+        elif opt in ("-v", "--version"):
+            VERSION = arg
+        elif opt == "--jar":
+            JAR_FILE = arg
+        elif opt == "--src_dir":
+            SRC_DIR = arg
+        elif opt == "--dest_dir":
+            DEST_DIR = arg
 
-	# The order is very important due to some dependencies between scripts, so specify the dependency order here.
-	FIRST_IN_FILES = [
+    # The order is very important due to some dependencies between scripts, so specify the dependency order here.
+    FIRST_IN_FILES = [
         "JSLITE.prototype.js",
         "JSLITE.js",
         "JSLITE.Element.js",
@@ -55,8 +55,8 @@ def main(argv):
         "JSLITE.Observer.js"
     ]
 
-	MINIFIED_SCRIPT = "JSLITE_" + VERSION + ".min.js"
-	COPYRIGHT = '''\
+    MINIFIED_SCRIPT = "JSLITE_" + VERSION + ".min.js"
+    COPYRIGHT = '''\
         /*
          * jsLite {VERSION!s}
          *
@@ -67,62 +67,62 @@ def main(argv):
          */
     '''.format(**locals())
 
-	PORT = "22"
-	DEST_REMOTE = "/path/to/dir"
-	USERNAME = getpass.getuser()
+    PORT = "22"
+    DEST_REMOTE = "/path/to/dir"
+    USERNAME = getpass.getuser()
 
-	try:
-		print("Creating minified script...\n")
+    try:
+        print("Creating minified script...\n")
 
         # Write to a buffer.
-		content = [textwrap.dedent(COPYRIGHT)]
+        content = [textwrap.dedent(COPYRIGHT)]
 
-		genny = (FIRST_IN_FILES + [os.path.basename(filepath) for filepath in glob.glob(SRC_DIR + "JSLITE*.js") if os.path.basename(filepath) not in FIRST_IN_FILES])
+        genny = (FIRST_IN_FILES + [os.path.basename(filepath) for filepath in glob.glob(SRC_DIR + "JSLITE*.js") if os.path.basename(filepath) not in FIRST_IN_FILES])
 
-		if len(genny) == 2:
-			print("OPERATION ABORTED: No JSLITE source files were found in the specified source directory. Check your path?")
-			sys.exit(1)
+        if len(genny) == 2:
+            print("OPERATION ABORTED: No JSLITE source files were found in the specified source directory. Check your path?")
+            sys.exit(1)
 
-		for script in genny:
-			begin = int(time.time())
-			content.append(subprocess.getoutput("java -jar " + JAR_FILE + " " + SRC_DIR + script))
-			end = int(time.time())
-			print("Script " + script + " minified in " + str(end - begin) + "s")
+        for script in genny:
+            begin = int(time.time())
+            content.append(subprocess.getoutput("java -jar " + JAR_FILE + " " + SRC_DIR + script))
+            end = int(time.time())
+            print("Script " + script + " minified in " + str(end - begin) + "s")
 
-		# This will overwrite pre-existing.
-		with open(DEST_DIR + "/" + MINIFIED_SCRIPT, mode="w", encoding="utf-8") as fp:
+        # This will overwrite pre-existing.
+        with open(DEST_DIR + "/" + MINIFIED_SCRIPT, mode="w", encoding="utf-8") as fp:
             # Flush the buffer (only perform I/O once).
-			fp.write("".join(content))
+            fp.write("".join(content))
 
-		resp = input("\nPush to server? [y|N]: ")
-		if resp in ["Y", "y"]:
-			resp = input("Username [" + USERNAME + "]:")
-			if resp != "":
-				USERNAME = resp
-			resp = input("Port [" + PORT + "]:")
-			if resp != "":
-				PORT = resp
-			resp = input("Remote destination [" + DEST_REMOTE + "]:")
-			if resp != "":
-				DEST_REMOTE = resp
+        resp = input("\nPush to server? [y|N]: ")
+        if resp in ["Y", "y"]:
+            resp = input("Username [" + USERNAME + "]:")
+            if resp != "":
+                USERNAME = resp
+            resp = input("Port [" + PORT + "]:")
+            if resp != "":
+                PORT = resp
+            resp = input("Remote destination [" + DEST_REMOTE + "]:")
+            if resp != "":
+                DEST_REMOTE = resp
 
-			p = subprocess.Popen(["scp", "-P", PORT, DEST_DIR + "/" + MINIFIED_SCRIPT, USERNAME + "@example.com:" + DEST_REMOTE])
-			sts = os.waitpid(p.pid, 0)
-			print("Minified script " + MINIFIED_SCRIPT + " pushed to " + DEST_REMOTE + " on remote server.")
-		else:
-			print("Minified script " + MINIFIED_SCRIPT + " created in " + DEST_DIR + "/")
+            p = subprocess.Popen(["scp", "-P", PORT, DEST_DIR + "/" + MINIFIED_SCRIPT, USERNAME + "@example.com:" + DEST_REMOTE])
+            sts = os.waitpid(p.pid, 0)
+            print("Minified script " + MINIFIED_SCRIPT + " pushed to " + DEST_REMOTE + " on remote server.")
+        else:
+            print("Minified script " + MINIFIED_SCRIPT + " created in " + DEST_DIR + "/")
 
-		print("Done!")
+        print("Done!")
 
-	except (KeyboardInterrupt):
-		# Control-c sent a SIGINT to the process, handle it.
-		print("\nProcess aborted!")
-		sys.exit(1)
+    except (KeyboardInterrupt):
+        # Control-c sent a SIGINT to the process, handle it.
+        print("\nProcess aborted!")
+        sys.exit(1)
 
 if __name__ == "__main__":
-	if len(sys.argv) == 1:
-		usage(0)
-		sys.exit(2)
+    if len(sys.argv) == 1:
+        usage(0)
+        sys.exit(2)
 
-	main(sys.argv[1:])
+    main(sys.argv[1:])
 
