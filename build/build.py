ec02de25a9f4bbe53be1d77a749d0d75ebf48943
Benjamin Toll <benjam72@yahoo.com>
2015-05-19 11:44:04 -0400

    Cleaned up build.py

--- build/build.py
+++ build/build.py
@@ -7,55 +7,60 @@ import sys
 import textwrap
 import time
 
-def usage(level):
-    if level == 0:
-        str = '''
-            Usage: python3 build.py [-hv | --help [--version --jar --src_dir --dest_dir]] [VERSION | JAR_FILE | SOURCE_DIRECTORY | DESTINATION_DIRECTORY]
-            Try `python3 build.py --help' for more information.
-        '''
-    elif level == 1:
-        str = '''
-            Usage:
-            --dest_dir      The location where the minified file will be moved, defaults to cwd.
-            --jar           The location of the jar file, defaults to '/usr/local/src/yuicompressor-2.4.8.jar'.
-            --src_dir       The location of the JSLITE source files, defaults to '/usr/local/www/public/dev/jslite/lib/js/'.
-            -v, --version   The version of the minified script, must be specified.
-        '''
+def usage():
+    str = '''
+        Usage:
+        --dest_dir      The location where the minified file will be moved, defaults to cwd.
+        --jar           The location of the jar file, defaults to '/usr/local/src/yuicompressor-2.4.8.jar'.
+        --src_dir       The location of the JSLITE source files, defaults to '/usr/local/www/public/dev/jslite/lib/js/'.
+        --version, -v   The version of the minified script, must be specified.
+    '''
     print(textwrap.dedent(str))
 
 def main(argv):
-    JAR_FILE = "/path/to/yuicompressor-2.4.8.jar"
-    SRC_DIR = "/path/to/src/js/"
-    DEST_DIR = "."
+    JAR_FILE = '/path/to/yuicompressor-2.4.8.jar'
+    DEST_DIR = '.'
+    SRC_DIR = ''
+    VERSION = ''
 
     try:
-        opts, args = getopt.getopt(argv, "hv:", ["help", "version=", "jar=", "src_dir=", "dest_dir="])
+        opts, args = getopt.getopt(argv, 'hv:', ['help', 'version=', 'jar=', 'src_dir=', 'dest_dir='])
     except getopt.GetoptError:
-        usage(0)
+        print('Error: Unrecognized flag.')
+        usage()
         sys.exit(2)
+
     for opt, arg in opts:
-        if opt in ("-h", "--help"):
-            usage(1)
+        if opt in ('-h', '--help'):
+            usage()
             sys.exit(0)
-        elif opt in ("-v", "--version"):
+        elif opt in ('-v', '--version'):
             VERSION = arg
-        elif opt == "--jar":
+        elif opt == '--jar':
             JAR_FILE = arg
-        elif opt == "--src_dir":
+        elif opt == '--src_dir':
             SRC_DIR = arg
-        elif opt == "--dest_dir":
+        elif opt == '--dest_dir':
             DEST_DIR = arg
+
+    if not (SRC_DIR):
+        print('Error: You must provide the location of the source files.')
+        sys.exit(2)
+
+    if not (VERSION):
+        print('Error: You must provide a version.')
+        sys.exit(2)
 
     # The order is very important due to some dependencies between scripts, so specify the dependency order here.
     FIRST_IN_FILES = [
-        "JSLITE.prototype.js",
-        "JSLITE.js",
-        "JSLITE.Element.js",
-        "JSLITE.Composite.js",
-        "JSLITE.Observer.js"
+        'JSLITE.prototype.js',
+        'JSLITE.js',
+        'JSLITE.Element.js',
+        'JSLITE.Composite.js',
+        'JSLITE.Observer.js'
     ]
 
-    MINIFIED_SCRIPT = "JSLITE_" + VERSION + ".min.js"
+    MINIFIED_SCRIPT = 'JSLITE_' + VERSION + '.min.js'
     COPYRIGHT = '''\
         /*
          * jsLite {VERSION!s}
@@ -67,61 +72,61 @@ def main(argv):
          */
     '''.format(**locals())
 
-    PORT = "22"
-    DEST_REMOTE = "/path/to/dir"
+    PORT = '22'
+    DEST_REMOTE = '~'
     USERNAME = getpass.getuser()
 
     try:
-        print("Creating minified script...\n")
+        print('Creating minified script...\n')
 
         # Write to a buffer.
         content = [textwrap.dedent(COPYRIGHT)]
 
-        genny = (FIRST_IN_FILES + [os.path.basename(filepath) for filepath in glob.glob(SRC_DIR + "JSLITE*.js") if os.path.basename(filepath) not in FIRST_IN_FILES])
+        genny = (FIRST_IN_FILES + [os.path.basename(filepath) for filepath in glob.glob(SRC_DIR + 'JSLITE*.js') if os.path.basename(filepath) not in FIRST_IN_FILES])
 
         if len(genny) == 2:
-            print("OPERATION ABORTED: No JSLITE source files were found in the specified source directory. Check your path?")
+            print('OPERATION ABORTED: No JSLITE source files were found in the specified source directory. Check your path?')
             sys.exit(1)
 
         for script in genny:
             begin = int(time.time())
-            content.append(subprocess.getoutput("java -jar " + JAR_FILE + " " + SRC_DIR + script))
+            content.append(subprocess.getoutput('java -jar ' + JAR_FILE + ' ' + SRC_DIR + script))
             end = int(time.time())
-            print("Script " + script + " minified in " + str(end - begin) + "s")
+            print('Script ' + script + ' minified in ' + str(end - begin) + 's')
 
         # This will overwrite pre-existing.
-        with open(DEST_DIR + "/" + MINIFIED_SCRIPT, mode="w", encoding="utf-8") as fp:
+        with open(DEST_DIR + '/' + MINIFIED_SCRIPT, mode='w', encoding='utf-8') as fp:
             # Flush the buffer (only perform I/O once).
-            fp.write("".join(content))
+            fp.write(''.join(content))
 
-        resp = input("\nPush to server? [y|N]: ")
-        if resp in ["Y", "y"]:
-            resp = input("Username [" + USERNAME + "]:")
-            if resp != "":
+        resp = input('\nPush to server? [y|N]: ')
+        if resp in ['Y', 'y']:
+            resp = input('Username [' + USERNAME + ']:')
+            if resp != '':
                 USERNAME = resp
-            resp = input("Port [" + PORT + "]:")
-            if resp != "":
+            resp = input('Port [' + PORT + ']:')
+            if resp != '':
                 PORT = resp
-            resp = input("Remote destination [" + DEST_REMOTE + "]:")
-            if resp != "":
+            resp = input('Remote destination [' + DEST_REMOTE + ']:')
+            if resp != '':
                 DEST_REMOTE = resp
 
-            p = subprocess.Popen(["scp", "-P", PORT, DEST_DIR + "/" + MINIFIED_SCRIPT, USERNAME + "@example.com:" + DEST_REMOTE])
+            p = subprocess.Popen(['scp', '-P', PORT, DEST_DIR + '/' + MINIFIED_SCRIPT, USERNAME + '@example.com:' + DEST_REMOTE])
             sts = os.waitpid(p.pid, 0)
-            print("Minified script " + MINIFIED_SCRIPT + " pushed to " + DEST_REMOTE + " on remote server.")
+            print('Minified script ' + MINIFIED_SCRIPT + ' pushed to ' + DEST_REMOTE + ' on remote server.')
         else:
-            print("Minified script " + MINIFIED_SCRIPT + " created in " + DEST_DIR + "/")
+            print('Minified script ' + MINIFIED_SCRIPT + ' created in ' + DEST_DIR + '/')
 
-        print("Done!")
+        print('Done!')
 
     except (KeyboardInterrupt):
         # Control-c sent a SIGINT to the process, handle it.
-        print("\nProcess aborted!")
+        print('\nProcess aborted!')
         sys.exit(1)
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     if len(sys.argv) == 1:
-        usage(0)
+        usage()
         sys.exit(2)
 
     main(sys.argv[1:])
